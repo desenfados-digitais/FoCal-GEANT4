992c4f6b4bae46720c3cc65d8adc9c53ba681d17
marco <78539460+desenfados-digitais@users.noreply.github.com>
2023-03-09 18:22:12 -0300

    Add files via upload

---
+++ sim/action.hh
@@ -0,0 +1,18 @@
+#ifndef ACTION_HH
+#define ACTION_HH
+
+#include "generator.hh"
+
+#include "G4VUserActionInitialization.hh"
+
+class MyActionInitialization : public G4VUserActionInitialization
+{
+public:
+    MyActionInitialization();
+    ~MyActionInitialization();
+
+    virtual void Build() const;
+
+};
+
+#endif
